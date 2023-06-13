59197898aba3f454466cbfb81b4074eb91764531
FelipeEnne <felipeenne@gmail.com>
2023-06-13 20:34:55 -0300

    add use ajax

--- script.js
+++ script.js
@@ -1,13 +1,34 @@
-const userLocation = document.querySelector(".location");
-const btn = document.querySelector(".btn");
+//ajax
 
-btn.addEventListener("click", () => {
-  navigator.geolocation.getCurrentPosition(showPosition);
-});
+const btn = document.getElementById("btn");
+btn.addEventListener("click", getUsers);
 
-function showPosition(position) {
-  userLocation.innerHTML = `
-    Latitude: ${position.coords.latitude} <br>
-    Longitude: ${position.coords.longitude}
-    `;
+function getUsers(e) {
+  e.preventDefault();
+
+  const http = new XMLHttpRequest();
+
+  http.open("GET", "users.json", true);
+  http.onload = function () {
+    if (this.status === 200) {
+      //console.log(this.responseText);
+
+      const users = JSON.parse(this.responseText);
+
+      let output = "";
+      users.forEach((user) => {
+        output += `
+        <hr>
+        <ul>
+          <li>id: ${user.id}</li>
+          <li>name: ${user.name}</li>
+          <li>age: ${user.age}</li>
+          <li>email: ${user.email}</li>
+        </ul>
+        `;
+      });
+      document.getElementById("users").innerHTML = output;
+    }
+  };
+  http.send();
 }
